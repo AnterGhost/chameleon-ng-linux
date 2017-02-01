d8aa016b5c610c709cdb4363f28d4e829f929e69
aixxe <me@aixxe.net>
2017-02-01 04:11:21 +0000

    Cut down Renderer class to bare minimum.
    * Will add everything back later.
    
    Signed-off-by: aixxe <me@aixxe.net>

--- src/Renderer.hpp
+++ src/Renderer.hpp
@@ -1,22 +1,11 @@
 #pragma once
 
-#include <windows.h>
-
-#include "imgui.h"
-#include "imgui_impl_dx9.h"
-
 class Renderer {
 	private:
-		bool ready = false; 
 		bool active = false;
-		HWND window = nullptr;
 	public:
-		~Renderer();
-
-		bool IsReady() const;
-		bool IsActive() const;
-		bool Initialize(HWND, IDirect3DDevice9*);
-		bool HandleInputMessage(UINT, WPARAM, LPARAM);
+		bool IsActive();
+		void SetActive(bool);
 };
 
 extern Renderer renderer;
